60166127994d912d460266083cbb498afd9ac8e7
ReaGol <reutgoldin@gmail.com>
2022-12-09 15:19:20 +0200

    folder hierarchy

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import fs from "fs";
+import http from "http";
+import { dataPath, getUsers, createUser } from "./utils.js";
+
+const PORT = "8000";
+const HOST = "localhost";
+const app = express();
+app.use(express.json());
+const server = http.createServer((req, res) => {});
+
+app.get("/users", (req, res) => {
+  const users = getUsers();
+  res.send(users);
+});
+
+
+
+app.listen(8000, () => {
+  console.log("listening on port 8000");
+});
